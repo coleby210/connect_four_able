112fc864935b75d058531be95c161b7895451044
Coleby Kent <coleby210@gmail.com>
2016-01-28 13:01:56 -0800

    Latest commit

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var Game = function(){
-  this.board = [
-  [nil, nil, nil, nil, nil, nil],
-  [nil, nil, nil, nil, nil, nil],
-  [nil, nil, nil, nil, nil, nil],
-  [nil, nil, nil, nil, nil, nil],
-  [nil, nil, nil, nil, nil, nil],
-  [nil, nil, nil, nil, nil, nil],
-  [nil, nil, nil, nil, nil, nil]
-  ];
+  this.board = {
+  "column-0": [0, 0, 0, 0, 0, 0],
+  "column-1": [0, 0, 0, 0, 0, 0],
+  "column-2": [0, 0, 0, 0, 0, 0],
+  "column-3": [0, 0, 0, 0, 0, 0],
+  "column-4": [0, 0, 0, 0, 0, 0],
+  "column-5": [0, 0, 0, 0, 0, 0],
+  "column-6": [0, 0, 0, 0, 0, 0]
+  };
 };
